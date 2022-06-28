3d59af0cc9d1789277dcf98c9975cc919b2ae1b0
kealeigh <kealeigh.usiak@spindance.com>
2022-06-28 14:28:34 -0400

    up to date

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import LoadScript from "vue-plugin-load-script";
+// import GoogleAuth from 'vue-google-oauth2'
+// import Vue from 'vue';
+// const gauthOption = {
+//     clientId: '117184943709978921122.apps.googleusercontent.com',
+//     scope: 'profile email',
+//     prompt: 'select_account'
+// };
+// Vue.use(GoogleAuth, gauthOption);
+
+
 
 const app = createApp(App)
 app.use(LoadScript);

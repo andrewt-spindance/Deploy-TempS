e02569a436734141fcd9ebe2b8f3681481b3bfc6
jacobi-spindance <jacob.irons@spindance.com>
2022-06-29 10:50:28 -0400

    Adding ability to fetch data from API using Axios

--- src/views/ViewAll.vue
+++ src/views/ViewAll.vue
@@ -1,14 +1,39 @@
-<script setup lang="ts">
-import RoomTable from "@/components/RoomTableComp.vue";
-import RoomView from "@/components/Room.vue";
-import Room2 from "@/components/Room copy.vue";
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import type { room } from '@/datatypes'
 
-// export default {
-//   components: {
-//     RoomView,
-//     RoomTable: RoomTable,
-//   },
-// };
+ export default class ViewAll extends Vue{
+
+  results: Array<room> = [];
+
+  mounted(): void {
+    axios.request({
+      method: "GET",
+      url: "https://xz2anw50qb.execute-api.us-east-1.amazonaws.com/prod/",
+    })
+    .then((response: AxiosResponse) => response.data)
+    .then((data: any) => {
+      let rooms = data.body;
+      for (let i = 0; i < rooms.length; i++) {
+        let tmp: room;
+        console.log(rooms[i]);
+
+        this.results.push({
+          name: rooms[i].chipId.S,
+          temperature: rooms[i].payload.M.temperature.N,
+          humidity: rooms[i].payload.M.humidity.N,
+          timestamp: rooms[i].payload.M.readingTime.S,
+        });
+      }
+      
+      for(let i = 0; i < this.results.length; i++){
+        console.log(this.results[i]);
+      }
+    });
+  }
+};
 </script>
 
 <!-- ">
